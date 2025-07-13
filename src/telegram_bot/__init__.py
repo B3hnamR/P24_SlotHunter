cce0bb7ca018777793c48a26868c57fc6bdb4e75
Behnam Rjd <24478741+B3hnamR@users.noreply.github.com>
2025-07-13 15:24:37 +0330

    bug fixes
    bug fixes

--- src/telegram_bot/__init__.py
+++ src/telegram_bot/__init__.py
@@ -1,27 +1,11 @@
-from .bot import SlotHunterBot
-from .handlers import TelegramHandlers
-from .menu_handlers import MenuHandlers
-from .callback_handlers import CallbackHandlers
-from .admin_menu_handlers_fixed import AdminMenuHandlers
+from .bot import NewSlotHunterBot as SlotHunterBot
+from .unified_handlers import UnifiedTelegramHandlers
+from .simple_menu import SimpleMenuHandlers
 from .messages import MessageFormatter
-from .access_control import access_control
-from .user_roles import user_role_manager, UserRole
-from .admin_handlers import TelegramAdminHandlers
-from .decorators import admin_required, user_required
-from .utils import TelegramUtils
 
 __all__ = [
     'SlotHunterBot',
-    'TelegramHandlers', 
-    'MenuHandlers',
-    'CallbackHandlers',
-    'AdminMenuHandlers',
-    'MessageFormatter',
-    'access_control',
-    'user_role_manager',
-    'UserRole',
-    'TelegramAdminHandlers',
-    'admin_required',
-    'user_required',
-    'TelegramUtils'
+    'UnifiedTelegramHandlers',
+    'SimpleMenuHandlers', 
+    'MessageFormatter'
 ]
